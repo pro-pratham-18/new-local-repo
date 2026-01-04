42344d32ff2f4867fc0d423ab1432990f6edba65
pro-pratham-18 <itz.prathathamesh18@gmail.com>
2026-01-04 14:33:41 +0530

    max ele code added

---
+++ bluebeast.cpp
@@ -0,0 +1,35 @@
+
+#include<algorithm>
+#include<climits>
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cout<<"Enter The Number Of Elements for an array:";
+    cin>>a;
+    int arr[a];
+
+    for(int i=0;i<a;i++){
+        cout<<"Enter The "<<i<<"th Index Element:"<<endl;
+        cin>>arr[i];
+    }
+
+    cout<<"The Array Formed:"<<endl;
+
+    for(int i=0;i<a;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    int mx = INT_MIN;
+
+    for(int i=0;i<a;i++){
+        mx=max(mx,arr[i]);
+    }
+
+    cout<<"The maximum element is:"<<mx<<endl;
+
+    return 0;
+}
